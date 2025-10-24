7700af2b6f571a86c49d5a435837dcc571e4e739
CGBerreteaga <cgberreteaga@icloud.com>
2025-10-24 14:14:22 -0400

    3: Restricted screen to 60 FPS (main.py::7-8,20

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from constants import *
 def main():
     print("Starting Asteroids!")
     pygame.init()
+    game_clock = pygame.time.Clock() 
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     exit_game = False
+
 
     while not exit_game:
         screen.fill((0, 0, 0))  # Clear screen with black
@@ -13,6 +17,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = game_clock.tick(60)/1000  # Cap the frame rate at 60 FPS
 
 if __name__ == "__main__":
     main()
