6886da16489159c8fd75928bbbbe0a991c23c69b
CGBerreteaga <cgberreteaga@icloud.com>
2025-10-24 14:06:33 -0400

    1: Added pygame.init, screen display, and screen refresh

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from constants import *
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    exit_game = False
+
+    while not exit_game:
+        screen.fill((0, 0, 0))  # Clear screen with black
+        pygame.display.flip()  # Update the full display Surface to the screen
 
 
 if __name__ == "__main__":
